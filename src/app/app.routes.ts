ff116f664700751b89ef7ec524c1d78b61eaa7ca
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-11 11:35:17 +0100

    feat: auth routes added and placeholders for other routes added for future use

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/jobs/search',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.routes')
+        .then(m => m.AUTH_ROUTES)
+    },
+    {
+        path: 'jobs',
+        loadChildren: () => import('./features/jobs/jobs.routes')
+        .then(m => m.JOBS_ROUTES)
+    },
+    {
+        path: 'applications',
+        canActivate: [authGuard],
+        loadChildren: () => import('./features/applications/applications.routes')
+        .then(m => m.APPLICATIONS_ROUTES)
+    },
+    {
+        path: 'profile',
+        canActivate: [authGuard],
+        loadChildren: () => import('./features/profile/profile.routes')
+        .then(m => m.PROFILE_ROUTES)
+    },
+    {
+        path: 'favorites',
+        canActivate: [authGuard],
+        loadChildren: () => import('./features/favorites/favorites.routes')
+        .then(m => m.FAVORITES_ROUTES)
+    },
+    {
+        path: '**',
+        redirectTo: '/auth/login'
+    }
+];
