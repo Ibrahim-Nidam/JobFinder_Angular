fdf903cacf1850ee649b7de903e9b65f35dc4f6b
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-11 11:42:42 +0100

    feat: storage service added to save the current user in a session storage

--- src/app/core/services/storage.ts
+++ src/app/core/services/storage.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { UserAuth } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Storage {
-  
+  private readonly USER_KEY = 'currentUser';
+
+  constructor(){}
+
+  saveUser(user: UserAuth): void {
+    sessionStorage.setItem(this.USER_KEY, JSON.stringify(user));
+  }
+
+  getUser(): UserAuth | null {
+    const userStr = sessionStorage.getItem(this.USER_KEY);
+    if(userStr){
+      return JSON.parse(userStr);
+    }
+    return null;
+  }
+
+  isLoggedIn(): boolean {
+    return this.getUser() !== null;
+  }
+
+  clearUser(): void {
+    sessionStorage.removeItem(this.USER_KEY);
+  }
 }
