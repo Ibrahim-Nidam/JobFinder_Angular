e9ee9247588d9cda9ed7374b19a3b50ab69b31b7
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-11 11:44:59 +0100

    feat: login logic and login component added

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,11 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  isLoading: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: Auth,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.errorMessage = '';
+    this.isLoading = true;
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password).subscribe({
+      next: (user) => {
+        console.log('login success', user);
+        this.router.navigate(['/jobs/search']);
+      },
+      error: (error) => {
+        console.error('login error', error);
+        this.errorMessage = error.message || 'error lors de la connex';
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+
+  hasError(fieldName: string, errorType: string): boolean {
+    const field = this.loginForm.get(fieldName);
+    return !!(field && field.hasError(errorType) && (field.dirty || field.touched));
+  }
 }
