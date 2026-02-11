254b27b43062d253c06401c1c460bd51b18486e0
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-11 11:39:02 +0100

    feat: job interface added

--- src/app/core/models/job.ts
+++ src/app/core/models/job.ts
@@ -1,2 +1,29 @@
 export interface Job {
+    id: number;
+    name: string;
+    contents: string;
+    short_name: string;
+    type: string;
+    publication_date: string;
+    model_type: string;
+    locations: Location[];
+    categories: Category[];
+    level: Level[];
+    tags: string[];
+    refs: {
+        landing_page: string;
+    }
+}
+
+export interface Location {
+    name: string;
+}
+
+export interface Category {
+    name: string;
+}
+
+export interface Level {
+    name: string;
+    short_name: string;
 }
