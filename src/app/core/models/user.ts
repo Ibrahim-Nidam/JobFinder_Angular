609681cee9de9ca784bdb0abb2e37111a8319ab1
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-11 11:36:11 +0100

    feat: user, userAuth interface added

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,2 +1,14 @@
 export interface User {
+    id?: number;
+    nom: string;
+    prenom: string;
+    email: string;
+    password?: string;
+}
+
+export interface UserAuth {
+    id: number;
+    nom: string;
+    prenom: string;
+    email: string;
 }
