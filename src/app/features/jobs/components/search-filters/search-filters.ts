3b6a53aa17abdf70a1e7e7fbb72274fd2274e3e8
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-12 11:54:35 +0100

    feat: job search filters component added

--- src/app/features/jobs/components/search-filters/search-filters.ts
+++ src/app/features/jobs/components/search-filters/search-filters.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+export interface SearchCriteria {
+  keyword: string;
+  location: string;
+}
 
 @Component({
   selector: 'app-search-filters',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-filters.html',
   styleUrl: './search-filters.css',
 })
 export class SearchFilters {
+  @Input() isLoading: boolean = false;
+  @Output() search = new EventEmitter<SearchCriteria>();
 
+  searchForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      keyword: [''],
+      location: ['']
+    });
+  }
+
+  onSubmit(): void {
+    const criteria: SearchCriteria = {
+      keyword: this.searchForm.value.keyword || '',
+      location: this.searchForm.value.location || ''
+    };
+    this.search.emit(criteria);
+  }
 }
