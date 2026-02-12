8e08fcd8f2240d49ea8fc27afebc482b6c81bf07
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-12 16:48:17 +0100

    feat: application routes added

--- src/app/features/applications/applications.routes.ts
+++ src/app/features/applications/applications.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from "@angular/router";
 
 export const APPLICATIONS_ROUTES: Routes = [
-    
+	{
+		path: '',
+		loadComponent: () => import('./pages/applications-list/applications-list')
+		.then(m => m.ApplicationsList)
+	}
 ]
