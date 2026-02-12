49da02e4505ea3b4c6762975d376da54b08f518f
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-12 15:08:09 +0100

    feat: profile root added

--- src/app/features/profile/profile.routes.ts
+++ src/app/features/profile/profile.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from "@angular/router";
 
 export const PROFILE_ROUTES: Routes = [
-    
+	{
+		path: '',
+		loadComponent: () => import('./pages/profile-edit/profile-edit')
+		.then(m => m.ProfileEdit)
+	}
 ]
