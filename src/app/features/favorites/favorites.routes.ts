be6b550ba90426bb544f556179f20d151b07435b
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-12 16:51:20 +0100

    feat: favorite component route added

--- src/app/features/favorites/favorites.routes.ts
+++ src/app/features/favorites/favorites.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from "@angular/router";
 
 export const FAVORITES_ROUTES: Routes = [
-    
+	{
+		path: '',
+		loadComponent: () => import('./pages/favorites-list/favorites-list')
+		.then(m => m.FavoritesList)
+	}
 ]
