8267bd8cf52b0e794fc3836eaf6f8140ac695cbb
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-12 11:42:50 +0100

    chore: ngrx entity added and new environment added to hide the api keys used

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  usaJobsApi: {
+    url: 'https://data.usajobs.gov/api/Search',
+    apiKey: 'Your API KEY',
+    userAgent: 'YOUR EMAIL'
+  }
+};
