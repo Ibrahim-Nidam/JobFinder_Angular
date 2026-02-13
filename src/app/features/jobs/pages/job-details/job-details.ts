677b3231f27003ad46bc41d30023c16964a2e58d
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-13 12:03:55 +0100

    fix: used new pipes and refactored code

--- src/app/features/jobs/pages/job-details/job-details.ts
+++ src/app/features/jobs/pages/job-details/job-details.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Footer } from '../../../../shared/components/footer/footer';
 import { Header } from '../../../../shared/components/header/header';
 import { Job } from '../../../../core/models/job';
+import { DateFormatPipe } from '../../../../shared/pipes/date-format.pipe';
+import { JobLocationPipe } from '../../../../shared/pipes/job-location.pipe';
 
 @Component({
   selector: 'app-job-details',
-  imports: [CommonModule, Header, Footer],
+  imports: [CommonModule, Header, Footer, DateFormatPipe, JobLocationPipe],
   templateUrl: './job-details.html',
   styleUrl: './job-details.css',
 })
 export class JobDetails {
   job: Job | null = null;
+  private router = inject(Router);
 
-  constructor(private router: Router) {
+  constructor() {
     const navigationState = this.router.getCurrentNavigation()?.extras.state as { job?: Job } | undefined;
     const historyState = history.state as { job?: Job } | undefined;
 
@@ -23,20 +26,5 @@ export class JobDetails {
 
   onBackToSearch(): void {
     this.router.navigate(['/jobs/search']);
-  }
-
-  getLocation(): string {
-    return this.job?.locations && this.job.locations.length > 0
-      ? this.job.locations[0].name
-      : 'Non specifie';
-  }
-
-  formatDate(dateString: string): string {
-    const date = new Date(dateString);
-    return date.toLocaleDateString('fr-FR', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
   }
 }
